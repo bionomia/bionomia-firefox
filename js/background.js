835e8d5124d9264fcf4e5a7e6c6d8f4989bcd4cd
David Shorthouse <davidpshorthouse@gmail.com>
2026-06-26 19:30:08 -0400

    Update manifest and align with Chrome extension

--- js/background.js
+++ js/background.js
@@ -1,80 +1,29 @@
-/*global chrome, Object, alert */
-var BionomiaBackground = (function($, window, document) {
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch(request.method) {
 
-  "use strict";
-
-  var _private = {
-
-    vars: {
-      gbifID: 0
-    },
-
-    receiveMessages: function() {
-      var self = this;
-
-      browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        switch(request.method) {
-
-          case 'bn_gbifID':
-
-            browser.tabs.query({active : true, currentWindow : true}, function(tab) {
-              tab = tab[0];
-              $.ajax({
-                type: "GET",
-                url: "https://api.bionomia.net/occurrence/" + request.params.gbifID + ".jsonld",
-                dataType: "json"
-              }).done(function(data) {
-                sendResponse({});
-                browser.tabs.sendMessage(tab.id, { method : "bn_occurrence", params : { data: data } });
-                return true;
-              })
-              .fail(function(data) {
-                sendResponse({});
-                browser.tabs.sendMessage(tab.id, { method : "bn_occurrence", params : { data: { message: "error"} } });
-                return true;
-              });
-            });
-          break;
-
-        case 'bn_gbifDatasetKey':
-          browser.tabs.query({active : true, currentWindow : true}, function(tab) {
-            tab = tab[0];
-            $.ajax({
-              type: "GET",
-              url: "https://api.bionomia.net/dataset/" + request.params.gbifDatasetKey + ".json",
-              dataType: "json"
-            }).done(function(data) {
-              sendResponse({});
-              browser.tabs.sendMessage(tab.id, { method : "bn_dataset", params : { data: data } });
-              return true;
-            })
-            .fail(function(data) {
-              sendResponse({});
-              browser.tabs.sendMessage(tab.id, { method : "bn_dataset", params : { data: { message: "error"} } });
-              return true;
-            });
+    case 'bn_gbifID':
+      browser.tabs.query({active : true, currentWindow : true}, (tabs) => {
+        fetch("https://api.bionomia.net/occurrence/" + request.params.gbifID + ".jsonld")
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            browser.tabs.sendMessage(tabs[0].id, { method : "bn_occurrence", params : { data: data } });
+            sendResponse();
+          })
+          .catch((error) => {
+            browser.tabs.sendMessage(tabs[0].id, { method : "bn_occurrence", params : { data: { message: "error"} } });
+            sendResponse();
           });
-        break;
-
-          default:
-            sendResponse({});
-        }
       });
-    }
+    break;
 
-  };
+    default:
+      sendResponse({});
+  }
+  return true;
+});
 
-  return {
-    init: function() {
-      _private.receiveMessages();
-    }
-  };
-
-}(jQuery, window, document));
-
-$(function() {
-  BionomiaBackground.init();
-  browser.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    browser.tabs.sendMessage(details.tabId, { method : "bn_flush" });
-  });
+browser.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+  browser.tabs.sendMessage(details.tabId, { method : "bn_flush" });
 });
--- js/content.js
+++ js/content.js
@@ -1,4 +1,5 @@
-/*global chrome, Object, alert */
+/*global browser, Object, alert */
+
 var Bionomia = (function($, window, document) {
 
   "use strict";
@@ -10,65 +11,70 @@ var Bionomia = (function($, window, document) {
       recorded: "",
       identified: "",
       cited: "",
-      people: "",
       timeout: 125
+    },
+
+    setDefaults: function() {
+      SVGInject.setOptions({
+        useCache: true,
+        beforeInject: function(img, svg) {
+          var hasPeople = true
+          $.each(svg.querySelectorAll('text'), function() {
+            if (this.textContent === "404 not found") {
+              hasPeople = false;
+            }
+          });
+          if (hasPeople) {
+            return svg;
+          } else {
+            return document.createElement("span");
+          }
+        }
+      });
     },
 
     receiveMessages: function() {
       var self = this, checkExist;
 
       browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
         switch(request.method) {
+
           case 'bn_flush':
-            self.flushAll();
+            self.flushVars();
             self.setGBIFidentifier(self.getGBIFidentifier());
+            self.createDatasetBadge();
             self.sendMessage();
           break;
+
           case 'bn_occurrence':
             $.each(request.params.data.recorded, function() {
-              self.vars.recorded += "<p>";
+              self.vars.recorded += "<p class=\"g-flex g-items-center\">";
               self.vars.recorded += self.makeName(this);
               self.vars.recorded += "</p>";
             });
             $.each(request.params.data.identified, function() {
-              self.vars.identified += "<p>";
+              self.vars.identified += "<p class=\"g-flex g-items-center\">";
               self.vars.identified += self.makeName(this);
               self.vars.identified += "</p>";
             });
             $.each(request.params.data.associatedReferences, function() {
               self.vars.cited += self.makeCited(this);
             });
-            checkExist = setInterval(function() {
-              if ($('header') && $('header').length) {
-                $('.bn-attribution').remove();
-                self.makeOccurrenceHTML();
-                clearInterval(checkExist);
-              }
-            }, 125);
+            self.createOccurrence();
           break;
-          case 'bn_dataset':
-          if (request.params.data["message"] !== undefined && request.params.data.message === "error") {
-            $('.bn-dataset').remove();
-          } else {
-            self.vars.people = request.params.data.users_count;
-            self.createDatasetButton();
-            self.adjustDatasetCounter();
-          }
-          break;
+
         }
         sendResponse({});
         return true;
       });
     },
 
-    flushAll: function() {
+    flushVars: function() {
       this.vars.gbifIdentifier = 0;
       this.vars.recorded = "";
       this.vars.identified = "";
       this.vars.cited = "";
-      this.vars.people = "";
-      $('.bn-dataset').remove();
-      $('.bn-attribution').remove();
     },
 
     getGBIFidentifier: function() {
@@ -86,117 +92,85 @@ var Bionomia = (function($, window, document) {
       this.vars.gbifIdentifier = id;
     },
 
-    createDatasetButton: function() {
-      var self = this, checkExist = "";
+    createDatasetBadge: function() {
+      var self = this, uuid_pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i, checkExist = "";
 
-      if ($('.bn-dataset') === null || $('.bn-dataset').length === 0) {
-        if (this.vars.gbifIdentifier !== 0 && !$.isNumeric(this.vars.gbifIdentifier)) {
-          checkExist = setInterval(function() {
-            if ($('#tabsScrollable') && $('#tabsScrollable').length) {
-              if ($('.bn-dataset').length === 0) {
-                self.makeDatasetHTML();
-              }
-              if (self.vars.people || self.vars.people === 0) {
-                self.adjustDatasetCounter();
-              }
-              clearInterval(checkExist);
-            }
-          }, 125);
-        }
+      if (this.vars.gbifIdentifier.match(uuid_pattern)) {
+        checkExist = setInterval(function() {
+          if ($('.bn-dataset').length === 0) {
+            self.makeDatasetBadgeHTML();
+          } else {
+            clearInterval(checkExist);
+          }
+        }, this.vars.timeout);
       }
     },
 
-    adjustDatasetCounter: function() {
-      $('.bn-dataset-counter').html(this.numberWithCommas(this.vars.people));
-      if (parseInt(this.vars.people,10) === 1) {
-        $('.gb-button--brand--bionomia-label').text(browser.i18n.getMessage("person"));
+    makeDatasetBadgeHTML: function() {
+      var html = "<div class=\"bn-dataset\">" +
+                  "<a href=\"https://bionomia.net/dataset/" + this.vars.gbifIdentifier + "\">" +
+                    "<img src=\"https://api.bionomia.net/dataset/" + this.vars.gbifIdentifier + "/badge.svg\" class=\"bn-badge\" alt=\"Bionomia dataset badge\" width=\"210\" height=\"20\">" +
+                  "</a>" +
+                "</div>";
+      $("article h1").next("div").append(html);
+      if ($(".bn-badge").length > 0) {
+        SVGInject($(".bn-badge")[0]);
       }
-    },
-
-    numberWithCommas: function(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
 
     makeName: function(data) {
       var response = "";
       response += data.name;
       if (data["sameAs"].includes("Q")) {
-        response += " <img src=\"" + browser.runtime.getURL("images/wikidata_16x16.png") + "\" width=\"16\" height=\"16\" alt=\"iD icon\" border=\"0\">";
+        response += " <img src=\"" + browser.runtime.getURL("images/wikidata_16x16.png") + "\" width=\"16\" height=\"16\" alt=\"iD icon\" border=\"0\" class=\"bn-logo\">";
       } else {
-        response += " <img src=\"" + browser.runtime.getURL("images/orcid_16x16.gif") + "\" width=\"16\" height=\"16\" alt=\"iD icon\" border=\"0\">";
+        response += " <img src=\"" + browser.runtime.getURL("images/orcid_16x16.gif") + "\" width=\"16\" height=\"16\" alt=\"iD icon\" border=\"0\" class=\"bn-logo\">";
       }
       response += " <a href=\"" + data["sameAs"] + "\">" + data["sameAs"] + "</a><br>";
       return response;
     },
 
-/*
-REMOVED for now until semantics are better handled
-    makeAttributor: function(data, owner) {
-      var response = "";
-      if (data["sameAs"] === owner) {
-        response += "<span style=\"font-size:x-small\">" + browser.i18n.getMessage("claimed_by") + ":";
-      } else {
-        response += "<span style=\"font-size:x-small\">" + browser.i18n.getMessage("attributed_by") + ":";
-      }
-      response += data.name;
-      if (data["sameAs"].includes("Q")) {
-        response += " <img src=\"" + browser.runtime.getURL("images/wikidata_16x16.png") + "\" width=\"12\" height=\"12\" alt=\"iD icon\" border=\"0\">";
-      } else {
-        response += " <img src=\"" + browser.runtime.getURL("images/orcid_16x16.gif") + "\" width=\"12\" height=\"12\" alt=\"iD icon\" border=\"0\">";
-      }
-      response += " <a href=\"" + data["sameAs"] + "\">" + data["sameAs"] + "</a>";
-      response += "</span>";
-      return response;
-    },
-*/
-
     makeCited: function(data) {
       var citation = data["description"] ? data["description"] : "";
-      return "<p class=\"bionomia-citation\">" + citation + " <a href=\"" + data["@id"] + "\">" + data["@id"] + "</a></p>";
+      return "<p class=\"bn-citation\">" + citation + " <a href=\"" + data["@id"] + "\">" + data["@id"] + "</a></p>";
+    },
+
+    createOccurrence: function() {
+      var self = this, checkExist = "";
+
+      if ($.isNumeric(this.vars.gbifIdentifier) && (this.vars.recorded || this.vars.identified || this.vars.cited)) {
+        checkExist = setInterval(function() {
+          if ($('.bn-attribution').length === 0) {
+            self.makeOccurrenceHTML();
+          } else {
+            clearInterval(checkExist);
+          }
+        }, this.vars.timeout);
+      }
     },
 
     makeOccurrenceHTML: function() {
       var title = "";
       if (this.vars.recorded) {
         title = browser.i18n.getMessage("collected_by");
-        $("header").append("<div class=\"bn-attribution\"><h4>" + title + "</h4>" + this.vars.recorded + "</div>");
+        $("article h1").next("div").children().last().append("<div class=\"bn-attribution\"><h4 class=\"g-font-semibold\">" + title + "</h4>" + this.vars.recorded + "</div>");
       }
       if (this.vars.identified) {
         title = browser.i18n.getMessage("identified_by");
-        $("header").append("<div class=\"bn-attribution\"><h4>" + title + "</h4>" + this.vars.identified + "</div>");
+        $("article h1").next("div").children().last().append("<div class=\"bn-attribution\"><h4 class=\"g-font-semibold\">" + title + "</h4>" + this.vars.identified + "</div>");
       }
       if (this.vars.cited) {
         title = browser.i18n.getMessage("cited_by");
-        $("header").append("<div class=\"bn-attribution\"><h4>" + title + "</h4>" + this.vars.cited + "</div>");
+        $("article h1").next("div").children().last().append("<div class=\"bn-attribution\"><h4 class=\"g-font-semibold\">" + title + "</h4>" + this.vars.cited + "</div>");
       }
-    },
-
-    makeDatasetHTML: function() {
-      var html = "", throbber = browser.runtime.getURL("images/ajax-loader.gif");
-      html = "<li class=\"tab tab-right ng-scope bn-dataset\">" +
-                "<span>" +
-                  "<a href=\"https://bionomia.net/dataset/" + this.vars.gbifIdentifier + "\" class=\"gb-button--brand gb-button--bionomia\">" +
-                    "<span class=\"bn-dataset-counter\"><img src=\"" + throbber + "\" /></span>" +
-                    "<span class=\"gb-button--brand--bionomia-label\">" + browser.i18n.getMessage("people") + "</span>" +
-                  "</a>" +
-                "</span>" +
-              "</li>";
-      $("#tabsScrollable").find("div.tabs__actions ul").append(html);
-      $("#tabsFixedContainer").find("div.tabs__actions ul").append(html);
     },
 
     sendMessage: function() {
       var self = this,
-          message = {},
-          uuid_pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-      if (self.vars.gbifIdentifier !== 0) {
-        if ($.isNumeric(self.vars.gbifIdentifier)) {
-          message = { gbifID : self.vars.gbifIdentifier };
-          browser.runtime.sendMessage({ method : "bn_gbifID", params : message });
-        } else if (self.vars.gbifIdentifier.match(uuid_pattern)) {
-          message = { gbifDatasetKey : self.vars.gbifIdentifier };
-          browser.runtime.sendMessage({ method : "bn_gbifDatasetKey", params : message });
-        }
+          message = {};
+      if (self.vars.gbifIdentifier !== 0 && $.isNumeric(self.vars.gbifIdentifier)) {
+        message = { gbifID : self.vars.gbifIdentifier };
+        browser.runtime.sendMessage({ method : "bn_gbifID", params : message });
       }
     }
 
@@ -204,9 +178,10 @@ REMOVED for now until semantics are better handled
 
   return {
     init: function() {
+      _private.setDefaults();
       _private.receiveMessages();
       _private.setGBIFidentifier(_private.getGBIFidentifier());
-      _private.createDatasetButton();
+      _private.createDatasetBadge();
       _private.sendMessage();
     }
   };
